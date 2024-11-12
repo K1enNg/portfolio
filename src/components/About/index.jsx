b4f3ea31d648d557dbdb2052fffa350ef468c67d
Kinkin <hiimkien2702@gmail.com>
2024-11-12 14:18:55 -0500

    update about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons/faHtml5';
+import { faCss3 } from '@fortawesome/free-brands-svg-icons/faCss3';
+import { faJava, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import './index.scss'
+
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 3000);
+      
+        return () => clearTimeout(timer);
+      }, []);
+
   return (
-    <div className="about-page">
+    <>
+        <div className="container about-page">
         <div className="text-zone">
             <h1>
                 <AnimatedLetters
+                    letterClass={letterClass}
                     strArray={['A','b','o','u','t',' ','m','e']}
                     idx={15}
                 />
@@ -20,9 +38,34 @@ const About = () => {
                 I am passionate about creating intuitive and engaging user interfaces and am excited to learn from experienced professionals in the field.</p>
             <p>I am seeking an internship where I can further develop my technical skills and contribute to innovative projects. 
                 I am enthusiastic about leveraging my knowledge and passion for front-end development to help create exceptional web experiences.</p>
-            <p></p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#f07f07" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#11a0d9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#dce305" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#1f11d9" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faNodeJs} color="#11bf2c" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+          </div>
         </div>
     </div>
+    <Loader type='pacman'/>
+    </>
+
   )
 }
 
